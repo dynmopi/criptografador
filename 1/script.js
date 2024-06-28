666e28e3de7d9c6ea117ac700df4297402cbe902
Danrley Maranhão <maranhaodanrley@gmail.com>
2024-06-28 14:52:46 -0300

    segundo-script

--- 1/script.js
+++ 1/script.js
@@ -6,38 +6,7 @@ function criptografar(){
     let resultado = "";
     let tradutor;
 
-while (c < frase.length){
-let verificador = frase[c];
-    if (verificador == "A" || verificador == "a" || verificador == "L" || verificador == "l" || verificador == "W"){
-        letra = "@";
-    } else if (verificador == "B" || verificador == "b" || verificador == "M" || verificador == "m" || verificador == "w"){
-        letra = "#";
-    } else if (verificador == "C" || verificador == "c" || verificador == "N" || verificador == "n" || verificador == "X"){
-        letra = "$"
-    } else if (verificador == "D" || verificador == "d" || verificador == "O" || verificador == "o" || verificador == "x"){
-        letra = "%"
-    } else if (verificador == "E" || verificador == "e" || verificador == "P" || verificador == "p" || verificador == "Y"){
-        letra = "&"
-    } else if (verificador == "F" || verificador == "f" || verificador == "Q" || verificador == "q" || verificador == "y"){
-        letra = "*"
-    } else if (verificador == "G" || verificador == "g" || verificador == "R" || verificador == "r" || verificador == "Z"){
-        letra = "ç"
-    } else if (verificador == "H" || verificador == "h" || verificador == "S" || verificador == "s" || verificador == "z"){
-        letra = "8"
-    } else if (verificador == "I" || verificador == "i" || verificador == "T" || verificador == "t"){
-        letra = "n"
-    } else if (verificador == "J" || verificador == "j" || verificador == "U" || verificador == "u"){
-        letra = "l"
-    } else if (verificador == "K" || verificador == "k" || verificador == "V" || verificador =="v"){
-        letra = "!"
-    } else {
-        letra = "";
-    }
-        resultado = tradutor + letra;
-        c++;
-        console.log(resultado)
-        
-    }
+    
     return tradutor;
 }
 
---
+++ script.js
@@ -0,0 +1,37 @@
+// A letra "e" é convertida para "enter"
+// A letra "i" é convertida para "imes"
+// A letra "a" é convertida para "ai"
+// A letra "o" é convertida para "ober"
+// A letra "u" é convertida para "ufat"
+
+let textArea = document.getElementById('input').value;
+
+//objetos
+let substituir = {
+    'a':'ai',
+    'e':'enter',
+    'i':'imes',
+    'o':'ober',
+    'u':'ufat',
+}
+
+
+function criptografar(textArea, substituir){
+    console.log(typeof(textArea))
+    let resultado = '';
+    let i = 0;
+    while (i < textArea.length){
+        let letra = textArea[i];
+        if (substituir.hasOwnProperty(letra)){
+            resultado += substituir[letra];
+        } else {
+            resultado += letra;
+        }
+        i++
+    }
+    return resultado
+}
+
+ let textoCriptografado = criptografar(textArea.toLowerCase(), substituir);
+ console.log(`criptografado ${textoCriptografado}`);
+
